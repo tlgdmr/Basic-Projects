605a1d5e3d82cae5702a359f665240878b9a6ce5
Tolga Demir <tolga_demir2015@outlook.com>
2022-05-08 18:24:56 +0200

    Basic Project

---
+++ Finding Vowel/Finding Vowel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Finding_Vowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> vowel = new List<char>();
+            vowel.Add('a');
+            vowel.Add('e');
+            vowel.Add('i');
+            vowel.Add('o');
+            vowel.Add('u');
+
+            int count = 0;
+
+            Console.WriteLine("Write a sentence or a word");
+            string input = Console.ReadLine();
+
+            String[] sentence = input.Split(" ");
+
+            string divideTheSentenceIntoWords;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != null)
+                {
+                    divideTheSentenceIntoWords = sentence[i];
+                    for (int k = 0; k < sentence[i].Length; k++)
+                    {
+                        char allWords = divideTheSentenceIntoWords[k];
+                        for (int l = 0; l < 5; l++)
+                        {
+                            if (vowel[l] == allWords)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Square Of Numbers/Square Of Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Square_Of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the last number");
+            int input = int.Parse(Console.ReadLine());
+
+            int squareOfAllNumbers = 0;
+
+            int sumOfNumbers = 0;
+
+            for (int i = 1; i <= input; i++)
+            {
+                squareOfAllNumbers += (i * i);
+            }
+
+
+            for (int i = 1; i <= input; i++)
+            {
+                sumOfNumbers += i;
+            }
+            int squareOfSumOfTheNumbers = sumOfNumbers * sumOfNumbers;
+
+            Console.WriteLine(squareOfSumOfTheNumbers - squareOfAllNumbers);
+
+
+        }
+    }
+}
